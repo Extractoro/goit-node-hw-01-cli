d9e5d2b688f6b18aabd90e71a2b159976fae9a22
Vadym Tytarenko (Extractoro) <67538058+Extractoro@users.noreply.github.com>
2022-08-02 20:51:40 +0300

    done

--- index.js
+++ index.js
@@ -1,4 +1,43 @@
-console.log(process.argv);
-const [, , a, b] = process.argv;
+const contacts = require("./contacts.js");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-console.log(Number(a) - Number(b));
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const allContacts = await contacts.listContacts();
+      console.table(allContacts);
+      break;
+
+    case "get":
+      const getUserById = await contacts.getContactById(id);
+      console.table(getUserById);
+      break;
+
+    case "add":
+      const addUser = await contacts.addContact(name, email, phone);
+      console.table(addUser);
+      break;
+
+    case "remove":
+      const removeUserById = await contacts.removeContact(id);
+      console.table(removeUserById);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
